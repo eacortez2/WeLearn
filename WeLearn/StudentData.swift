99000410a66015f20fe430bcd2c4f970c6ca115c
Ashley <eacortez2@pipeline.sbcc.edu>
2015-02-21 03:40:09 -0800

    Added Fake Student Data

---
+++ WeLearn/StudentData.swift
@@ -0,0 +1,12 @@
+//
+//  StudentData.swift
+//  WeLearn2
+//
+//  Created by turbs on 2/21/15.
+//  Copyright (c) 2015 com. All rights reserved.
+//
+
+import Foundation
+
+
+let fakeData = [FireBaseUser(name: "Ashley", major: "Computer Science", photo: " ", bio: "Java 4 Lyfe"), FireBaseUser(name: "Zach", major: "Computer Science", photo: " ", bio: "Hackathon Ninja...Cute and Jewish"), FireBaseUser(name: "Nidhal", major: "Computer Science", photo: " ", bio: "Tunisia->'Murica. Here to tutor YOU!"), FireBaseUser(name: "Russ", major: "Computer Science", photo: " ", bio: "Surfing pics always get the ladies. Swipe forever.")]
