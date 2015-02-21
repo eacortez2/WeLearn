bbf012e3b64a9710833b2e12f8029b4d93587a94
Ashley <eacortez2@pipeline.sbcc.edu>
2015-02-21 00:58:20 -0800

    User class

---
+++ WeLearn/WeLearn2/user.swift
@@ -0,0 +1,35 @@
+//
+//  user.swift
+//  hackPolyClasses
+//
+//  Created by turbs on 2/20/15.
+//  Copyright (c) 2015 turbs. All rights reserved.
+//
+
+import Foundation
+
+
+class user {
+    
+    var name: String
+    var major: String
+    var photo: String   //must be stored as a string for the file name
+    var rep: Int
+    var location: [Double]
+    var bio: String
+    var badges: [String]
+    var radius: Int
+    
+    
+    init(name: String, major: String, photo:String, bio: String){
+        self.name = name
+        self.major = major
+        self.photo = photo
+        self.rep = 0
+        self.location = []
+        self.bio = bio
+        self.badges = []
+        self.radius = 5  //default is a 5 mile radius
+    }
+
+}
