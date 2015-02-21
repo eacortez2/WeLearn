6f765b701ac3b5f0ad2f97fc1d4d4677d7989b29
RussFenenga <russfenenga@gmail.com>
2015-02-21 12:16:29 -0800

    Trying TO Fix

--- WeLearn/WeLearn2/AroundMeViewController.swift
+++ WeLearn/WeLearn2/AroundMeViewController.swift
@@ -14,7 +14,7 @@ class AroundMeViewController: UITableViewController, UITableViewDataSource {
     
     @IBOutlet var tableViewData: UITableView!
     //temporary for testing before FireBase hookup
-    let students: [FireBaseUser] = fakeData
+    //let students: [FireBaseUser] = fakeData
     //var nearByPeople:[FireBaseUser] = []
     var ref = Firebase(url:"https://welearnhackpoly.firebaseio.com/users")
     var count: Int = 0
